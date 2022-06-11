9fbf6a7820e5b6cf9cf63603b74b7038d134ea6d
Daniel Rodríguez <daniel.rodriguebr98@gmail.com>
2022-06-11 10:00:44 +0200

    bet2peer service

--- client/src/services/bet2peer-service.js
+++ client/src/services/bet2peer-service.js
@@ -4,13 +4,22 @@ import * as Bet2PeerJSON from "../contracts/Bet2Peer.json";
 
 export class Bet2PeerService {
   contractABI = Bet2PeerJSON.abi;
-  contractAddress = "";
+  contractAddress = "0xaFAd47eaE4bc9F55Ca6B06Aef8e9105e183CBe7a";
   web3;
-  constructor(web3Service) {
-    this.web3 = web3Service.getWeb3();
+  fatherContract;
+  constructor() {
+    
+  }
+  
+  configureService = async (web3Service) => {
+    await web3Service.getWeb3();
+    this.fatherContract = web3Service.eth.Contract(this.contractABI, this.contractAddress);
   }
 
-  createBet(matchId, quantity) {}
+  createBet = async (bet) => {
+    const minimumCounterBet = bet.quantity*bet.quota;
+    this.fatherContract.methods.createBet(bet.match.id, bet.result, bet.quantity, minimumCounterBet)
+  }
 
   getCurrentMatches() {}
   //18165994, 18165993, 18166002, 18165998, 18166001, 18165997
